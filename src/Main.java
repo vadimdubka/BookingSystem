3b6b66cfa1bfe9a9e61e6106ced41f18bf8d403c
vadimdubka <vadimdubka@gmail.com>
2017-04-05 02:54:20 +0300

    cinema booking system

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import logic.user.UserSession;
-
-public class Main {
-    public static void main(String[] args) {
-        UserSession session = new UserSession();
-        session.startUserSession();
-    }
-}
